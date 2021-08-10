91965ad382b5c5096468323ce0b5f08000da1956
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-10 09:37:44 -0600

    Add post fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const sendOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(order)
+  })
+  .then(response => response.json())
+}
