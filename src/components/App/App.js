ba71ffaeadadc073c410f4dbfbf9dbc6340e237f
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-10 10:52:57 -0600

    Add submit delete handler in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import {getOrders, sendOrder} from '../../apiCalls';
+import {getOrders, sendDelete, sendOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -23,13 +23,22 @@ const App = () => {
     .catch(err => console.error(err))
   }
 
+  const submitDelete = (id) => {
+    sendDelete(id)
+    .then(message => {
+      const filtered = orders.filter(order => order.id !== id)
+      setOrders(filtered)
+    })
+    .catch(err => console.error(err))
+  }
+
   return (
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
         <OrderForm submitOrder={submitOrder}/>
       </header>
-      <Orders orders={orders}/>
+      <Orders orders={orders} submitDelete={submitDelete}/>
     </main>
   );
 }
